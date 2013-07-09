c171a00a8e9acc510f21732645574ef28cd5eb4b
Dustin Larimer <dustinlarimer@gmail.com>
2013-07-09 18:30:15 -0500

    Debugging statik

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var statik = require('statik');
 var server = statik.createServer();
 server.listen(process.env.PORT || 1337);*/
 
-var statik = require('statik')
+var statik = require('statik');
 var port = process.env.PORT || 3333
-statik(port)
+console.log port
+statik();
