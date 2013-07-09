1f570fccf3d5fdafe123aadd7dab97eaa89f907d
Dustin Larimer <dustinlarimer@gmail.com>
2013-07-09 17:04:47 -0500

    Config'd for Heroku, pt.3

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
-var statik = require('statik');
-var server = statik.createServer();
-server.listen(process.env.PORT || 1337);
+var http = require('http')
+var port = process.env.PORT || 3000
+var server = exports.server = http.createServer(app).listen(port, function() {
+  console.log('<+++ Client app serving ' + port + ' +++>')
+});
+
+//var statik = require('statik');
+//var server = statik.createServer();
+//server.listen(process.env.PORT || 1337);
