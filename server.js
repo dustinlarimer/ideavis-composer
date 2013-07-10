6367b80458b1c3c89b5e1b588fc2bd7894c725d7
Dustin Larimer <dustinlarimer@gmail.com>
2013-07-09 22:43:38 -0500

    taking jkresner's method for a spin

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
+console.log('Launching server')
+
+var express = require('express')
+var app = express()
+var port = process.env.PORT || 3333
+
+app.configure(function() {
+  app.use(express["static"](__dirname + '/public'));
+  return app.use(express.bodyParser());
+});
+
+exports.startServer = function() {
+  console.log("Now serving port: " + port);
+  return app.listen(port);
+};
+
+exports.startServer()
+
 /*
 var http = require('http')
 var port = process.env.PORT || 3333
@@ -9,7 +27,7 @@ var server = http.createServer().listen(port, function() {
 /*
 var statik = require('statik');
 var server = statik.createServer();
-server.listen(process.env.PORT || 1337);*/
+server.listen(process.env.PORT || 1337);
 
 console.log('Starting server')
 
@@ -18,3 +36,5 @@ var port = process.env.PORT || 3333;
 statik(port);
 
 console.log('Now serving port ' + port)
+
+*/
